64181216951cc619313dfc3d4f618576bcad7234
Cristian Beldea <hello@capitalmerce.com>
2023-03-25 22:25:12 +0200

    Added page redirect

--- app/next.config.js
+++ app/next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/ro/blog',
+        permanent: false,
+      },
+    ]
+  },
   reactStrictMode: true,
   compress: true,
   output: 'standalone',
