d50c745ec46eddc03b6fb226d75cfb956218dae6
Cristian Beldea <hello@capitalmerce.com>
2023-03-26 20:31:06 +0300

    Added react gtm package

--- app/src/pages/_app.js
+++ app/src/pages/_app.js
@@ -1,18 +1,13 @@
 import '@/styles/globals.css'
 import '@/styles/main.css'
 import { ThemeProvider } from 'next-themes';
-import Script from 'next/script';
-import GTag from '../../components/gtag';
+import TagManager from 'react-gtm-module';
 
 function MyApp({ Component, pageProps }) {
   const { theme } = pageProps;
   return (
     <ThemeProvider defaultTheme={theme}>
-      <GTag
-        measurementId="G-GWMRVJ2DK8"
-        transportUrl="https://measure.coursessor.com"
-        firstPartyCollection={true}
-      />
+      <TagManager gtmId="GTM-T96R7R2" />
       <Component {...pageProps} />
     </ThemeProvider>
   );
