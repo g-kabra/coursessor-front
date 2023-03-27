1ae0112626908abfc65eaa5bce38b0e13c09e400
Cristian Beldea <hello@capitalmerce.com>
2023-03-27 19:01:45 +0300

    Minor changes

--- app/src/pages/_app.js
+++ app/src/pages/_app.js
@@ -1,7 +1,8 @@
-import '@/styles/globals.css'
-import '@/styles/main.css'
+import '@/styles/globals.css';
+import '@/styles/main.css';
 import { ThemeProvider } from 'next-themes';
 import TagManager from 'react-gtm-module';
+import Script from 'next/script';
 import GoogleAnalytics from '../../components/Analytics';
 
 function MyApp({ Component, pageProps }) {
@@ -21,3 +22,4 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
