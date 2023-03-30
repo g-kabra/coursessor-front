7604bbf96c4b491ff30845b51f49cf4dfb69acef
Cristian Beldea <hello@capitalmerce.com>
2023-03-30 15:04:09 +0300

    API test

---
+++ app/components/Invoke.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+export default function Invoke() {
+  const [userInput, setUserInput] = useState('');
+  const [response, setResponse] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('https://api.coursessor.com/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ userInput })
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setResponse(data.message);
+      } else {
+        throw new Error('Request failed.');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  return (
+    <div className='flex flex-col justify-center'>
+      <div className='pt-10'>{response}</div>
+      <form onSubmit={handleSubmit} className = "flex flex-col justify-between">
+        <label>
+          User input:
+          <input type="text" value={userInput} onChange={handleChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
