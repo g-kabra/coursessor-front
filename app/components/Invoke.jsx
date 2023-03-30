3caac462d896323e708ee375a3f5936429290ae6
Cristian Beldea <hello@capitalmerce.com>
2023-03-30 17:34:11 +0300

    API v1

--- app/components/Invoke.jsx
+++ app/components/Invoke.jsx
@@ -1,26 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Invoke() {
   const [userInput, setUserInput] = useState('');
   const [response, setResponse] = useState('');
+  const [typingIndex, setTypingIndex] = useState(0);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await fetch('https://api.coursessor.com/', {
+      const response = await fetch('https://4ywlduggzuit4edt67uaubypzy0pjrci.lambda-url.eu-central-1.on.aws/', {
         method: 'POST',
         mode: 'cors',
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json'
-        },
         body: JSON.stringify({ userInput })
       });
 
       if (response.ok) {
         const data = await response.json();
         setResponse(data.message);
+        setTypingIndex(0); // reset the typing index when a new response is received
       } else {
         throw new Error('Request failed.');
       }
@@ -29,6 +27,17 @@ export default function Invoke() {
     }
   };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTypingIndex((prevIndex) => {
+        if (prevIndex === response.length) {
+          clearInterval(interval); // stop the interval when typing is complete
+        }
+        return prevIndex + 1;
+      });
+    }, 50);
+    return () => clearInterval(interval); // cleanup the interval when the component unmounts
+  }, [response]);
 
   const handleChange = (event) => {
     setUserInput(event.target.value);
@@ -36,11 +45,10 @@ export default function Invoke() {
 
   return (
     <div className='flex flex-col items-center '>
-      <div className='pt-10'>{response}</div>
-      <form onSubmit={handleSubmit} className="flex flex-col justify-between">
+      <div className='p-10'>{response.substring(0, typingIndex)}</div>
+      <form onSubmit={handleSubmit} className="flex flex-col justify-between mt-15">
         <label>
-          User input:
-          <input type="text" value={userInput} onChange={handleChange} />
+          <input type="text" value={userInput} onChange={handleChange} className='bg-transparent' />
         </label>
         <button type="submit">Submit</button>
       </form>
